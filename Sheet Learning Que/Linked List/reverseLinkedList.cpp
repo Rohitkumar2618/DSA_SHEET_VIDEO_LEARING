3ce2c7c23543888c089aa4fdb2d0b4e189e1ac89
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-07-25 13:13:05 +0530

    25-07-24

---
+++ Sheet Learning Que/Linked List/reverseLinkedList.cpp
@@ -0,0 +1,61 @@
+//  ! Brute force approach using the two pointers
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+          ListNode* prev = NULL;
+            ListNode* curr = head;
+
+            while( curr != NULL){
+
+                  ListNode* nextNode = curr->next;
+                  curr->next = prev;
+                  prev = curr;
+                  curr = nextNode;
+            }
+        return prev;
+    }
+};
+
+
+
+
+// ! using the recursive method
+
+class Solution {
+public:
+
+
+  ListNode* reverseUsingRecursion (  ListNode* prev ,   ListNode*  curr){
+
+
+if( curr == NULL){
+    return prev;
+}
+
+
+ListNode* nextNode = curr->next;
+
+curr->next = prev;
+prev = curr;
+curr = nextNode;
+
+
+  ListNode*  recursionAns = reverseUsingRecursion ( prev,curr);
+
+  return recursionAns ; 
+
+  }
+
+    ListNode* reverseList(ListNode* head) {
+
+          ListNode*  prev = NULL;
+            ListNode*  curr = head;
+
+              return   reverseUsingRecursion(prev,curr);
+
+        
+    }
+};-
